17d7fab03dfedc6f53f69fe82e6c0652436cd6e1
isha2510 <ishatripathi80@gmail.com>
2019-11-23 14:19:44 +0530

    ag grid edit cell : added feature

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import GridExample from './test';
+function App() {
+  return (
+    <div className="App">
+
+      <GridExample/>
+
+    </div>
+
+  );
+}
+
+export default App;
